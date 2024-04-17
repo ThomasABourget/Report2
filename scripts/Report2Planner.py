dd3d5869fb2bdeb6546ff10c33ef4411399a904d
ThomasABourget <150868757+ThomasABourget@users.noreply.github.com>
2024-04-17 15:18:29 -0400

    Update Report2Planner.py

--- scripts/Report2Planner.py
+++ scripts/Report2Planner.py
@@ -18,21 +18,25 @@ if __name__ == '__main__':
 	# set a 10Hz frequency for this loop
 	loop_rate = rospy.Rate(10)
 	# try getting the most update transformation between the tool frame and the base frame
-		try:
-			trans = tfBuffer.lookup_transform("base", "fk_tooltip", rospy.Time())
-		except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-			print('Frames not available!!!')
-			loop_rate.sleep()
-			continue
+	try:
+		trans = tfBuffer.lookup_transform("base", "camera_color_optical_frame", rospy.Time())
+	except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+		print('Frames not available!!!')
+		loop_rate.sleep()
+		continue
 	
-	pt_in_tool = tf2_geometry_msgs.PointStamped()
-	pt_in_tool.header.frame_id = 'fk_tooltip'
-	pt_in_tool.header.stamp = rospy.get_rostime()
-	pt_in_tool.point.z= 0.1 # 10 cm away from flange
-	
-
 	sphere_params_msg = SphereParams()
+	pt_in_tool = tf2_geometry_msgs.PointStamped()
+	pt_in_tool.header.frame_id = 'camera_color_optical_frame'
+	pt_in_tool.header.stamp = rospy.get_rostime()
+	pt_in_tool.point.z = sphere_params_msg.zc # 10 cm away from flange
+	pt_in_tool.point.x = sphere_params_msg.xc
+	pt_in_tool.point.y = sphere_params_msg.zc
 
+	pt_in_base = tfBuffer.transform(pt_in_tool,'base', rospy.Duration(1.0))
+	print('Test point in the TOOL frame:  x= ', format(pt_in_tool.point.x, '.3f'), '(m), y= ', format(pt_in_tool.point.y, '.3f'), '(m), z= ', format(pt_in_tool.point.z, '.3f'),'(m)')
+	print('Transformed point in the BASE frame:  x= ', format(pt_in_base.point.x, '.3f'), '(m), y= ', format(pt_in_base.point.y, '.3f'), '(m), z= ', format(pt_in_base.point.z, '.3f'),'(m)')
+	print('-------------------------------------------------')
 
 	# define a plan variable
 	plan = Plan()
@@ -50,9 +54,9 @@ if __name__ == '__main__':
 	
 	plan_point2 = Twist()
 	# define a point away from the initial position
-	plan_point2.linear.x = sphere_params_msg.xc
-	plan_point2.linear.y = sphere_params_msg.yc
-	plan_point2.linear.z = sphere_params_msg.zc
+	plan_point2.linear.x = 0.115
+	plan_point2.linear.y = -0.798
+	plan_point2.linear.z = 0.331
 	plan_point2.angular.x = 1.57
 	plan_point2.angular.y = 0.0
 	plan_point2.angular.z = 0.0
